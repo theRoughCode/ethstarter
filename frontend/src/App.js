3c81e2d28c3724a758d6deb99d7c6aee5bab074d
Moustafa Ismael <m.ismael@nectarfinancial.com>
2017-10-14 02:29:19 -0400

    Material ui theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RaisedButton from 'material-ui/RaisedButton';
+import AppBar from 'material-ui/AppBar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+      <div className="ideas-page">
+        <AppBar title="EthStarter" iconElementLeft='' />
+        <RaisedButton label="Default" />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
